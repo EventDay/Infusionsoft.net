eb138814a3ec8f22c8acfd1e15001530d518d154
Scott Cate <me@scottcate.com>
2018-03-16 06:44:52 -0500

    IL Merge still needs strong.format - minor change

--- src/InfusionSoft/OAuthCustomerInfusionSoftConfiguration.cs
+++ src/InfusionSoft/OAuthCustomerInfusionSoftConfiguration.cs
@@ -27,7 +27,7 @@ namespace InfusionSoft
         {
             if (_Uri == null)
             {
-                _Uri = new Uri($"https://api.infusionsoft.com/crm/xmlrpc/v1?access_token={AccessToken}");
+                _Uri = new Uri(String.Format("https://api.infusionsoft.com/crm/xmlrpc/v1?access_token={0}", AccessToken));
             }
 
             return _Uri;
